0848183092e156994806aea2368db219fdba1313
janhavikendre <janhavikendre717@gmail.com>
2025-06-08 13:33:53 +0530

    ba-fashion

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -40,11 +40,7 @@ export default function Footer() {
           <div>
             <h3 className="text-lg font-semibold mb-4">Programs</h3>
             <ul className="space-y-2">
-              <li>
-                <Link href="/academics/programs" className="text-gray-300 hover:text-white transition-colors">
-                  B.A. Fashion Design
-                </Link>
-              </li>
+              
               <li>
            
               </li>
